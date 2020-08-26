ffae36070cbca6ffb3b9278cbf7edad07a2089d5
Pablo Arboleda <68712714+parbole@users.noreply.github.com>
2020-08-26 00:09:50 -0500

    Ejercicio terminado, se adiciona interfaz con Jframe

--- src/ascensor/Ascensor1.java
+++ src/ascensor/Ascensor1.java
@@ -4,42 +4,62 @@ import javax.swing.JOptionPane;
 
 public class Ascensor1 {
 
-    private int build, down, actual, lugar;
-    private int piso = 1;
+    private int actual;
     private int total = 0;
+    private int pisos = 1;
+    private int sotanos = 0;
 
-    public Ascensor1(int build, int down, int actual, int lugar, int total) {
+    public Ascensor1(int up, int down, int actual, int total) {
         this.actual = actual;
-        this.build = build;
-        this.down = down;
-        this.lugar = lugar;
+        this.pisos = up;
+        this.sotanos = down;
         this.total = total;
     }
-
-    public void subir() {
-        String ps = JOptionPane.showInputDialog("Estas en el piso " + actual + " selecciona el piso al que deseas subir");
-        piso = Integer.parseInt(ps);
-        if (piso <= build) {
-            actual += piso - 1;
-            JOptionPane.showMessageDialog(null, "Ahora estas en el piso " + actual);
-        } else {
-            JOptionPane.showMessageDialog(null, "Este piso no existe");
+       
+    public void setPisoSubiendo() {
+        if(actual == -1){
+            actual = 1;
+        }
+        else if (actual < pisos) {
+            actual = actual + 1;
+        } 
+        else if(actual == pisos){
+            JOptionPane.showMessageDialog(null, "Esta en el ultimo piso, no puede subir más");
         }
     }
 
-    public void bajar() {
-        String ps = JOptionPane.showInputDialog("Estas en el piso " + actual + " selecciona el piso al que deseas bajar");
-        piso = Integer.parseInt(ps);
-        if (piso <= build) {
-            actual += piso - 1;
-            JOptionPane.showMessageDialog(null, "Ahora estas en el sotano " + actual);
-        } else {
-            JOptionPane.showMessageDialog(null, "Este piso no existe");
+    public void setPisoBajando() {
+        int numSotanos = sotanos * -1;
+        
+        if (sotanos == 0 && actual == 1) {
+            JOptionPane.showMessageDialog(null, "El edificio no tiene sotanos, no se puede bajar");
+        }
+        else if(actual == 1){
+            actual = -1;
+        }
+        else if (actual > numSotanos) {
+            actual = actual - 1;
+        }
+        else if(actual == numSotanos){
+            JOptionPane.showMessageDialog(null, "Esta en el ultimo sotano, no puede bajar más");
         }
     }
     
-    public void atendidos(){
-        total = build + down;
-        JOptionPane.showMessageDialog(null, "El total de pisos que atiende el ascensor es " + total);
+    public int getPisoActual(){
+        return actual;
     }
+    
+    public void setPisos(int pisos){
+        this.pisos = pisos;
+    }
+    
+    public void setSotanos(int sotanos){
+        this.sotanos = sotanos;
+    }
+    
+    public int getAtendidos(){
+        total = pisos + sotanos;
+        return total;
+    }
+    
 }
